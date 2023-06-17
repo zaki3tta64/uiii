3b60e3458724c3cdbdb2f33303e1064722b899ac
zaki3tta64 <71356575+zaki3tta64@users.noreply.github.com>
2023-06-17 15:44:44 +0300

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,47 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.FormLayout")
+                .setHost(host,"xui_ui_formlayout9")
+                .setLeft("15.238095238095237em")
+                .setTop("4.571428571428571em")
+                .setWidth("30.01904761904762em")
+                .setHeight("24.99047619047619em")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button21")
+                .setLeft("16em")
+                .setTop("35.80952380952381em")
+                .setCaption("Button")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input29")
+                .setLeft("20.571428571428573em")
+                .setTop("35.80952380952381em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input")
+            );
+            
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel37")
+                .setDock("width")
+                .setWidth("auto")
+                .setHeight("18em")
+                .setPosition("relative")
+                .setCaption("Relative Panel")
+                .setToggleBtn(true)
+                .setCloseBtn(true)
+                .setRefreshBtn(true)
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
